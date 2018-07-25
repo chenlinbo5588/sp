af3a1ef8a2e81e0eed54646d0444fcbaf7d84501
chenlinbo5588 <chenlinbo5588@163.com>
2018-07-25 10:47:56 +0800

    报修功能 实体文件

---
+++ application/entity/sp_repair.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$entity = array(
+'id' => 'int',
+'repair_type' => 'int',
+'resident_id' => 'int',
+'yezhu_id' => 'int',
+'yezhu_name' => 'varchar',
+'mobile' => 'varchar',
+'address' => 'varchar',
+'remark' => 'varchar',
+'photos' => 'text',
+'add_uid' => 'int',
+'edit_uid' => 'int',
+'add_username' => 'varchar',
+'edit_username' => 'varchar',
+'gmt_create' => 'int',
+'gmt_modify' => 'int'
+);
