5ab9d8a07cc00add15e438c0e918cee67a03b488
gang.li <772021606@qq.coom>
2018-12-19 14:47:18 +0800

    新闻开启关闭BUG修改

--- application/controllers/api/Cms.php
+++ application/controllers/api/Cms.php
@@ -15,41 +15,56 @@ class Cms extends Wx_Controller {
 	 */
 	public function getHomeInformation(){
 		$artileClassAssoc = $this->cms_service->getAssocDataTree();
-		
-		$notify = $this->Cms_Article_Model->getList(array(
-			'select' => 'id,image_url,article_title,publish_time,jump_url',
+		$notifyArticleClassInfo = $this->Cms_Article_Class_Model->getList(array(
 			'where' => array(
-				'ac_id' => $artileClassAssoc['通知公告']['id'],
-				'article_state' => CmsArticleStatus::$published
-			),
-			'order' => 'publish_time DESC',
-			'limit' => 5
+				'name' => '通知公告'
+			)
 		));
-		
+		if($notifyArticleClassInfo == 1){
+			$notify = $this->Cms_Article_Model->getList(array(
+				'select' => 'id,image_url,article_title,publish_time,jump_url',
+				'where' => array(
+					'ac_id' => $artileClassAssoc['通知公告']['id'],
+					'article_state' => CmsArticleStatus::$published,
+				),
+				'order' => 'publish_time DESC',
+				'limit' => 5
+			));	
+		}
 		$notify = $this->editInformation($notify);
-		
-		$news = $this->Cms_Article_Model->getList(array(
-			'select' => 'id,image_url,article_title,publish_time,jump_url',
+		$newsArticleClassInfo = $this->Cms_Article_Class_Model->getList(array(
 			'where' => array(
-				'ac_id' => $artileClassAssoc['新闻资讯']['id'],
-				'article_state' => CmsArticleStatus::$published
-			),
-			'order' => 'publish_time DESC',
-			'limit' => 5
+				'name' => '新闻资讯'
+			)
 		));
-		
+		if($newsArticleClassInfo[0]['status'] == 1){
+			$news = $this->Cms_Article_Model->getList(array(
+				'select' => 'id,image_url,article_title,publish_time,jump_url',
+				'where' => array(
+					'ac_id' => $artileClassAssoc['新闻资讯']['id'],
+					'article_state' => CmsArticleStatus::$published
+				),
+				'order' => 'publish_time DESC',
+				'limit' => 5
+			));
+		}
 		$news = $this->editInformation($news);
-		
-		$services = $this->Cms_Article_Model->getList(array(
-			'select' => 'id,image_url,article_title,publish_time,jump_url',
+		$servicesArticleClassInfo = $this->Cms_Article_Class_Model->getList(array(
 			'where' => array(
-				'ac_id' => $artileClassAssoc['生活服务']['id'],
-				'article_state' => CmsArticleStatus::$published
-			),
-			'order' => 'publish_time DESC',
-			'limit' => 5
+				'name' => '新闻资讯'
+			)
 		));
-		
+		if($servicesArticleClassInfo[0]['status'] == 1){
+			$services = $this->Cms_Article_Model->getList(array(
+				'select' => 'id,image_url,article_title,publish_time,jump_url',
+				'where' => array(
+					'ac_id' => $artileClassAssoc['生活服务']['id'],
+					'article_state' => CmsArticleStatus::$published
+				),
+				'order' => 'publish_time DESC',
+				'limit' => 5
+			));
+		}
 		$services = $this->editInformation($services);
 		
 		$artileList = array(
