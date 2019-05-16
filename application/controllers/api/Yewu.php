30c737bd87187bd3390b1cd5db89dd1878873a4a
ligangPC <772021606@qq.com>
2019-05-16 15:56:38 +0800

    接口修改

--- application/controllers/api/Yewu.php
+++ application/controllers/api/Yewu.php
@@ -27,7 +27,7 @@ class Yewu extends Wx_Tdkc_Controller {
 /*	  			$this->form_validation->set_rules('mobile','手机号码','required|valid_mobile');
 	  			$this->form_validation->set_rules('real_name','办证使用名字','required');*/
   			$this->form_validation->set_rules('yewu_describe','业务描述','max_length[255]');
-  			$this->form_validation->set_rules('service_area','服务区域','required|in_list['.implode(',',array_keys($serviceAreaList)).']');
+  			$this->form_validation->set_rules('service_area','测绘区域','required|in_list['.implode(',',array_keys($serviceAreaList)).']');
   			
   			if(!$this->form_validation->run()){
 				$this->jsonOutput2($this->form_validation->error_first_html());
@@ -45,14 +45,14 @@ class Yewu extends Wx_Tdkc_Controller {
   				'real_name' => $real_name,
   				'yewu_describe' => $yewu_describe,
   				'service_area' => $serviceAreaList[$service_area]['id'],
-  				'user_id' => $this->userInfo['id'],
+  				'user_id' => $this->userInfo['uid'],
   				'user_name' => $this->userInfo['name'],
   				'user_mobile' => $this->userInfo['mobile'],
 				'add_uid'	=>  $this->userInfo['id'],
 				'add_username'	=>  $this->userInfo['name'],
 				'status' => Operation::$submit,
 				'group_id' => $groupInfo['id'],
-				'company_name' => $company_name
+				//'company_name' => $company_name
 				//'accept_number' => $acceptNumber,
 			);
 
@@ -459,24 +459,38 @@ class Yewu extends Wx_Tdkc_Controller {
 
 		$yewuID = $this->postJson['yewu_id'];
 		$workCategory = $this->postJson['work_category'];
-		
-		if($yewuID){
+		$yewuName = $this-> postJson['yewu_name'];
+		for($i = 0;$i < 1; $i++){
+			//getBasicData
+			$workCategoryList = $this->basic_data_service->getTopChildList('工作类别');
 			
-			
-			$result = $this->Yewu_Model->updateByCondition(
-				array(
-					'status' => Operation::$accept,
-					'worker_name' => $this->userInfo['name'],
-					'worker_mobile' => $this->userInfo['mobile'],
-					'work_category' => $workCategory,	
-				),
-				array('where' => array('id' => $yewuID))
-			);
-			$this->yewu_service->addYewuDetail($this->userInfo,Operation::$accept,$yewuID);
-			if($result){
-				$this->jsonOutput2(RESP_SUCCESS);
+			$this->form_validation->set_data($this->postJson);
+
+  			$this->form_validation->set_rules('yewu_name','业务名称','required');
+  			$this->form_validation->set_rules('work_category','工作类别','required|in_list['.implode(',',array_keys($workCategoryList)).']');
+  			
+  			if(!$this->form_validation->run()){
+				$this->jsonOutput2($this->form_validation->error_first_html());
+				break;
 			}
-			
+			if($yewuID){
+				
+				$result = $this->Yewu_Model->updateByCondition(
+					array(
+						'yewu_name' => $yewuName,
+						'status' => Operation::$accept,
+						'worker_name' => $this->userInfo['name'],
+						'worker_mobile' => $this->userInfo['mobile'],
+						'work_category' => $workCategoryList[$workCategory]['id'],	
+					),
+					array('where' => array('id' => $yewuID))
+				);
+				$this->yewu_service->addYewuDetail($this->userInfo,Operation::$accept,$yewuID);
+				if($result){
+					$this->jsonOutput2(RESP_SUCCESS);
+				}
+				
+			}
 		}
 	}
 
@@ -491,7 +505,7 @@ class Yewu extends Wx_Tdkc_Controller {
 		extract($this->postJson,EXTR_OVERWRITE);
 		if($invoice_company && $invoice_no && $result){
 			$result = $this->Invoice_Model->_add(array(
-				'user_id' => $this->userInfo['id'],
+				'user_id' => $this->userInfo['uid'],
 				'invoice_company' => $invoice_company,
 				'invoice_no' => $invoice_no,
 				'address' => $address,
@@ -507,7 +521,7 @@ class Yewu extends Wx_Tdkc_Controller {
 	
 	
 	public function getInvoiceList(){
-		$invoiceList = $this->Invoice_Model->getList(array('where' => array('user_id' => $this->userInfo['id'])));
+		$invoiceList = $this->Invoice_Model->getList(array('where' => array('user_id' => $this->userInfo['uid'])));
 		$this->jsonOutput2(RESP_SUCCESS,array('invoiceList' => $invoiceList));
 
 	}
@@ -529,7 +543,7 @@ class Yewu extends Wx_Tdkc_Controller {
 			extract($this->postJson,EXTR_OVERWRITE);
 			$this->Invoice_Model->updateByCondition(
 				array(
-				'user_id' => $this->userInfo['id'],
+				'user_id' => $this->userInfo['uid'],
 				'invoice_company' => $invoice_company,
 				'invoice_no' => $invoice_no,
 				'address' => $address,
@@ -555,7 +569,11 @@ class Yewu extends Wx_Tdkc_Controller {
 				
 				$this->form_validation->set_rules('invoice_company','公司名称','required');
 				$this->form_validation->set_rules('invoice_no','税号','required|alpha_numeric');
-				
+				$invoiceNo = $this->postJson['invoice_no'];
+/*				if(strlen($invoiceNo != 15,)
+				if(15 == strlen($invoiceNo)){
+					$regioncode
+				}*/
 				if($type == 2){
 					$this->form_validation->set_rules('address','地址','required');
 					$this->form_validation->set_rules('mobile','电话','required');
